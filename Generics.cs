7251790533c8ad46ca2841db23670d77897bcef8
Rishabh28111995 <109350653+Rishabh28111995@users.noreply.github.com>
2023-07-03 22:32:13 +0530

    Add files via upload

---
+++ Generics.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace GenericsDemo
+{
+	//Generic means general form
+	//It does not specify a particular Data type
+	//We are using <> (angular bracket) with type name.
+	//Typename <T> T is your type parameter
+	//Generic allow us to a single class or method that can be
+	//used for diff. datatype.
+	//adv. : we can reuse code.
+	public class Generics<T>
+	{
+		public T data;
+		public Generics(T data)
+		{
+			this.data = data;
+			Console.WriteLine(this.data);
+		}
+		public void GetMethod(T num)
+		{
+			Console.WriteLine(num);
+		}
+	}
+	public class Generic
+	{
+		public void GenericMethod<T> (T num)
+		{
+			Console.WriteLine(num);
+		}
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace GenericsDemo;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Generics<int> generics = new Generics<int>(34);
+        Generics<string> generics1 = new Generics<string>("Rishabh");
+        Generics<double> generics2 = new Generics<double>(3.3444);
+        Generics<char> generics3 = new Generics<char>('a');
+        Generics<bool> generics4 = new Generics<bool>(true);
+        Generics<float> generics5 = new Generics<float>(3.48F);
+        generics.GetMethod(34);
+        generics1.GetMethod("Sharma");
+        generics2.GetMethod(29.39);
+        generics3.GetMethod('r');
+        generics4.GetMethod(false);
+        generics5.GetMethod(3.432F);
+
+        Generic generic = new Generic();
+        generic.GenericMethod<int>(324);
+        generic.GenericMethod<string>("Rishi");
+        generic.GenericMethod<double>(23.678);
+        generic.GenericMethod<char>('x');
+        generic.GenericMethod<bool>(true);
+        generic.GenericMethod<float>(3.678F);
+
+    }
+}
+
